17f9c1e4fdb3035d94977ef6d0653f2cf62ad527
Jarrett Lyon <64499632+probableactions@users.noreply.github.com>
2023-06-19 20:33:35 -0600

    Backtrace nodes traveled tp generate path

--- js/knight.js
+++ js/knight.js
@@ -18,35 +18,39 @@ const knightMoves = ([x1, y1], [x2, y2]) => {
 		[-1, 2],
 	];
 
-	let queue = [startPos];
-	const searched = new Set([startPos]);
-	let movesMade = 0;
+	const queue = [[startPos, 0]];
+	const searched = new Map();
+	searched.set(startPos, null);
+	let path = [[String(endPos)]];
 	while (queue.length) {
-		const next = [];
-		while (queue.length) {
-			const currentPosition = queue.shift();
-			if (String(currentPosition) === String(endPos)) {
-				return movesMade;
-			}
-			for (const direction of potentialDirections) {
-				const potentialNextPosition = [
-					currentPosition[0] + direction[0], // X coordinate
-					currentPosition[1] + direction[1], // Y coordinate
-				];
+		const [currentPosition, distance] = queue.shift();
 
-				// Ensure the move would be within the bounds of the board - need to check string value since they have different references
-				if (chessBoard.toString().includes(potentialNextPosition.toString())) {
-					if (!searched.has(potentialNextPosition)) {
-						searched.add(potentialNextPosition);
-						next.push(potentialNextPosition);
-					}
+		if (String(currentPosition) === String(endPos)) {
+			let current = endPos;
+			while (String(current) !== String(startPos)) {
+				let temp = searched.get(String(current));
+				path.unshift([searched.get(String(current))]);
+				current = temp;
+				path;
+			}
+
+			return { distance, path };
+		}
+		for (const direction of potentialDirections) {
+			const potentialNextPosition = [
+				currentPosition[0] + direction[0], // X coordinate
+				currentPosition[1] + direction[1], // Y coordinate
+			];
+
+			// Ensure the move would be within the bounds of the board - need to check string value since they have different references
+			if (chessBoard.toString().includes(potentialNextPosition.toString())) {
+				if (!searched.has(String(potentialNextPosition))) {
+					searched.set(String(potentialNextPosition), String(currentPosition));
+					queue.push([potentialNextPosition, distance + 1]);
 				}
 			}
 		}
-
-		movesMade += 1;
-		queue = next;
 	}
 };
 
-knightMoves([1, 1], [3, 5]);
+knightMoves([1, 1], [1, 5]);
